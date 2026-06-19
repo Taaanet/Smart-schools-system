946d4da78565d5b06bb6b64654ea422340f2f1cc
Taaanet <93224771+Taaanet@users.noreply.github.com>
2026-06-20 00:45:06 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for, send_file, make_response
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, send_file, make_response, flashfrom flask_cors import CORS
 from flask_mail import Mail, Message
 from datetime import datetime, timedelta, time
 import os
